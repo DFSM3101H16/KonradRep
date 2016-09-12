bbeffafa4eadc49d448dc8a9a5a1c2d40005d88f
KonradGA <kogian95@gmail.com>
2016-09-12 09:23:14 +0200

    Code

---
+++ UnityCode/Fysikk/Assets/Physics.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Physics : MonoBehaviour {
+    public float Mass;
+    
+    public Vector3 Velocity = new Vector3(0, 5, 10);
+
+    private Vector3 GravityEarth = new Vector3(0, -9.81f, 0);
+    private Vector3 KineticEnergy = new Vector3(0,0,0);
+    private float PotentialEnergy;
+    private Vector3 Gravity = new Vector3();
+
+    bool on = true;
+
+    // Use this for initialization
+    void Start() {
+
+        Gravity = GravityEarth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            setPosition();
+            setVelocity();
+        }
+    }
+    
+    void setPosition()
+    {
+        transform.position +=   Velocity * Time.deltaTime + 0.5f * Gravity * Time.deltaTime*Time.deltaTime;
+        
+    }
+
+    void setVelocity()
+    {
+        Velocity = Velocity + Gravity * Time.deltaTime;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        on = false;
+    }
+
+    /*
+    void setAirDrag()
+    {
+
+    }
+    
+    void setPEnergy()
+    {
+        
+        PotentialEnergy = -Mass * Position.y;
+    }
+    
+    void setKEnergy()
+    {
+        KineticEnergy = Mass * Velocity;
+    }
+    */
+}
